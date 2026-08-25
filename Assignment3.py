d5318d11cc06b66f4fafa4972e926ad7e476689b
Kartiki Jadhav <kartikijadhav2007@gmail.com>
2026-08-25 10:21:22 +0530

    Create Assignment3.py

---
+++ Assignment3.py
@@ -0,0 +1,8 @@
+subjects = ["Python", "Data Structures", "Database Management System", "Operating System"]
+print(subjects)
+
+subjects.append("Deld")
+print(subjects)
+
+subjects.remove("Operating System")
+print(subjects)
